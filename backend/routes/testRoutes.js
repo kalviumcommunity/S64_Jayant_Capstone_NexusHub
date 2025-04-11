1761f42e62d79abfcfe6e92895ef6ef944a1e0d1
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-04-11 11:07:57 +0530

    POST API

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { createTest } = require("../controllers/testController");
 const { getTestMessage } = require("../controllers/testController");
 
 const router = express.Router();
 
 router.get("/", getTestMessage);
+router.post("/create", createTest);
 
 module.exports = router;
