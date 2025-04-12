3e8b6ec47b6964a75ad8b18e0c96ca863a5dac00
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-04-12 15:25:52 +0530

    DB Schema created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./MongoDb");
-const testRoutes = require("./routes/testRoutes");
+const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
 
 dotenv.config();
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/test", testRoutes);
+app.use("/api", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
