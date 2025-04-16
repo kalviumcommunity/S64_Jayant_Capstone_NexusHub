2257ca81ac905ebf2bb7eb2ec59c26cce8557f38
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-04-17 01:53:51 +0530

    JWT Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
-// backend/models/userModel.js
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -18,5 +18,18 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 }, { timestamps: true });
+
+// Hash the password before saving the user
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = mongoose.model("User", userSchema);
