6878586ccfbc98cd40064bfea40f9128d08da728
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-04-20 20:13:50 +0530

    Frontend updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import Navbar from './components/Navbar';
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import About from './components/About'  
+import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
-      );
-    }
+  )
+}
 
-export default App;
-
+export default App
